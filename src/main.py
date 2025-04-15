2eb53425bcb41bac7432d929b9ba6f8e56b65b2d
AlexandruTarau <161336565+AlexandruTarau@users.noreply.github.com>
2025-04-15 13:42:34 +0200

    Experimenting with BM25

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from nltk.tokenize import wordpunct_tokenize
+from rank_bm25 import BM25Okapi
+from tqdm import tqdm
+
+tqdm.pandas()
 
 # === File Paths ===
 PATH_COLLECTION_DATA = 'subtask4b_collection_data.pkl'
@@ -21,7 +23,6 @@ print("Query dev data loaded:", df_query_dev.shape)
 # === Preprocessing Function ===
 stop_words = set(stopwords.words('english'))
 def preprocess(text):
-    #print("Preprocessing:", text[:100])
     tokens = wordpunct_tokenize(text.lower())
     tokens = [w for w in tokens if w.isalpha() and w not in stop_words]
     return ' '.join(tokens)
@@ -33,21 +34,22 @@ df_collection['text_clean'] = df_collection['text'].apply(preprocess)
 collection_texts = df_collection['text_clean'].tolist()
 cord_uids = df_collection['cord_uid'].tolist()
 
-# === Fit TF-IDF Vectorizer ===
-tfidf = TfidfVectorizer()
-tfidf_matrix = tfidf.fit_transform(collection_texts)
+# === BM25 === Tokenize the preprocessed text (space-separated string -> list of tokens)
+corpus_tokenized = [doc.split() for doc in df_collection['text_clean']]
+bm25 = BM25Okapi(corpus_tokenized)
 
-# === Function to Get Top-5 Predictions ===
-def get_top5_predictions(tweet_text):
-    query_clean = preprocess(tweet_text)
-    query_vec = tfidf.transform([query_clean])
-    cos_sim = cosine_similarity(query_vec, tfidf_matrix).flatten()
-    top5_idx = np.argsort(-cos_sim)[:5]
+# === Function to Get Top-5 Predictions using BM25 ===
+def get_top5_predictions_bm25(tweet_text):
+    query_tokens = preprocess(tweet_text).split()
+    scores = bm25.get_scores(query_tokens)
+    top5_idx = np.argsort(-scores)[:5]
     return [cord_uids[i] for i in top5_idx]
 
-# === Apply on Train and Dev ===
-df_query_train['tfidf_top5'] = df_query_train['tweet_text'].apply(get_top5_predictions)
-df_query_dev['tfidf_top5'] = df_query_dev['tweet_text'].apply(get_top5_predictions)
+
+# === Apply on Train and Dev using BM25 ===
+df_query_train['bm25_top5'] = df_query_train['tweet_text'].progress_apply(get_top5_predictions_bm25)
+df_query_dev['bm25_top5'] = df_query_dev['tweet_text'].progress_apply(get_top5_predictions_bm25)
+
 
 # === Evaluation Function ===
 def get_performance_mrr(data, col_gold, col_pred, list_k=[1, 5, 10]):
@@ -57,13 +59,13 @@ def get_performance_mrr(data, col_gold, col_pred, list_k=[1, 5, 10]):
         d_performance[k] = data['in_topx'].mean()
     return d_performance
 
-# === Evaluate ===
-results_train = get_performance_mrr(df_query_train, 'cord_uid', 'tfidf_top5')
-results_dev = get_performance_mrr(df_query_dev, 'cord_uid', 'tfidf_top5')
+# === Evaluate BM25 Predictions ===
+results_train_bm25 = get_performance_mrr(df_query_train, 'cord_uid', 'bm25_top5')
+results_dev_bm25 = get_performance_mrr(df_query_dev, 'cord_uid', 'bm25_top5')
 
-print("Results on the train set:", results_train)
-print("Results on the dev set:", results_dev)
+print("BM25 Results on the train set:", results_train_bm25)
+print("BM25 Results on the dev set:", results_dev_bm25)
 
-# === Export Predictions ===
-df_query_train[['post_id', 'tfidf_top5']].rename(columns={'tfidf_top5': 'preds'}).to_csv('predictions_train.tsv', sep='\t', index=False)
-df_query_dev[['post_id', 'tfidf_top5']].rename(columns={'tfidf_top5': 'preds'}).to_csv('predictions_dev.tsv', sep='\t', index=False)
+# === Export Predictions for BM25 ===
+df_query_train[['post_id', 'bm25_top5']].rename(columns={'bm25_top5': 'preds'}).to_csv('predictions_train.tsv', sep='\t', index=False)
+df_query_dev[['post_id', 'bm25_top5']].rename(columns={'bm25_top5': 'preds'}).to_csv('predictions_dev.tsv', sep='\t', index=False)
